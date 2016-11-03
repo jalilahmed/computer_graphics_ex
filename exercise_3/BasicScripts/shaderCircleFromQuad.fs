bb550238072cf761a5d31e35a26dd7e74d7faab3
Jalil Ahmed Siddiqui <jalilmaqsood@gmail.com>
2016-11-03 02:39:01 +0100

    Add files via upload

---
+++ exercise_3/BasicScripts/shaderCircleFromQuad.fs
@@ -0,0 +1,34 @@
+precision mediump float;
+
+//////////////////////////////////////////////////////////////
+// Exercise 3.3: Circle from Quad
+//  define a constant variable (uniform) to define the canvas size from the "outside"
+
+uniform vec2 canvasSize;
+
+
+void main(void)
+{
+	//////////////////////////////////////////////////////////////
+	// Exercise 3.3: Circle from Quad
+	// map the fragment coordinate gl_FragCoord into the range of [-1,1]
+	// discard all elements "outside" the radius length
+	// smooth the circle edge within [r-smoothMargin, r] by computing an appropriate alpha value
+
+	 float smoothMargin = .7;
+	 float r = 0.8;
+		
+	vec2 uv = gl_FragCoord.xy;
+	float map_u = ((uv[0]/canvasSize[0])*2.0) - 1.0;
+	float map_v = ((uv[1]/canvasSize[1]) * 2.0) - 1.0;
+	float a = map_u * map_u;
+	float b = map_v * map_v;
+	float c = r * r;
+	
+
+	if(a+b <= c){
+        	gl_FragColor = vec4(1.0,0,0, clamp(0.1, smoothMargin, r));
+	}    	
+	else{discard;}	
+	
+}
